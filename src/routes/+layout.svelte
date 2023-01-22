39849b7c5558d263369b9fc971ffc0d1f1dcc1aa
ohager <oliver@devbutze.com>
2023-01-22 18:27:23 -0300

    feat: added tailwindcss and daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,35 @@
 <script>
-  import { onMount } from 'svelte'
-  import { pwaInfo } from 'virtual:pwa-info'
+	import '../app.postcss';
+	import { onMount } from 'svelte';
+	import { pwaInfo } from 'virtual:pwa-info';
 
-  onMount(async () => {
-    if (pwaInfo) {
-      const { registerSW } = await import('virtual:pwa-register')
-      registerSW({
-        immediate: true,
-        onRegistered(r) {
-          // uncomment following code if you want check for updates
-          // r && setInterval(() => {
-          //    console.log('Checking for sw update')
-          //    r.update()
-          // }, 20000 /* 20s for testing purposes */)
-          console.log(`SW Registered: ${r}`)
-        },
-        onRegisterError(error) {
-          console.log('SW registration error', error)
-        }
-      })
-    }
-  })
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
 
-  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
+	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
 <svelte:head>
-  {@html webManifest}
+	{@html webManifest}
 </svelte:head>
 
 <main>
-  <slot />
+	<slot />
 </main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')],
+
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
+};
+
+module.exports = config;
